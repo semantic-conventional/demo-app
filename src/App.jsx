a52b60edc486893b5a8942528386854914ad24e9
Erik Hofer <erik-hofer@mail.de>
2023-06-01 10:07:25 +0200

    feat: add color choser (#5)
    Co-authored-by: Erik Hofer <erik.hofer@bmiag.de>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { colors } from "@semantic-conventional/demo-lib";
 import { createSignal, For } from 'solid-js';
+import styles from "./App.module.css"
 
 function App() {
+  const [selectedColorIndex, setSelectedColorIndex] = createSignal(0)
+
+  const selectedColorCode = () => colors[selectedColorIndex()].code
+
+  const isActive = (index) => index === selectedColorIndex()
+
   return (
-    <div>
-      <ul>
-        <For each={colors}>{color => <li>{color.name}</li>}</For>
-      </ul>
+    <div class={styles.container} style={{ "background-color": selectedColorCode() }}>
+      <div class={styles.card}>
+          <For each={colors}>{(color, index) => 
+            <div class={styles.option} onclick={() => setSelectedColorIndex(index)} active={index() === selectedColorIndex()}>
+              <div class={styles.circle} style={{ "background-color": color.code }}></div>
+              {color.name}
+            </div>
+          }</For>
+      </div>
     </div>
   );
 }
